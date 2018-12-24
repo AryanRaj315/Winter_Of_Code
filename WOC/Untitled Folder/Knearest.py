04bd2ebb29bbe05f1ddf3a5ce387b9e4ee4407f2
AryanRaj315 <41698113+AryanRaj315@users.noreply.github.com>
2018-12-24 23:18:10 +0530

    Update Knearest.py

--- WOC/Untitled Folder/Knearest.py
+++ WOC/Untitled Folder/Knearest.py
@@ -9,14 +9,17 @@ import matplotlib.pyplot as plt
 
 #   Implementing KNN  #
 
-class LinearRegression:
+class KNN:
     
     
-    def __init__(self, x, y, Lambda):
+    def __init__(self, x, y, k):
         
         x, y = self.shuffle_data(x, y)
         self.x = x
-        self.y = y        
+        self.y = y
+        self.k = k
+        cls = np.unique(self.y)
+    
         
     def shuffle_data(self, x, y):
         data = np.hstack((self.x, self.y))
@@ -31,5 +34,20 @@ class LinearRegression:
         data_std = np.std(self.data, axis=0)
         data_norm=(self.data-data_mean)/data_std
         return data_norm
+    
+    def euclideanDistance(self, point):
+        
+        distance =  np.sqrt(np.sum((self.x - point)**2, axis = 1))        
+        return np.argsort(distance)
+        
+     def k_neighbors(self):
+       
+        euc_dist = euclideanDistance(self , point)
+        neighbors = self.y[euc_dist]
+        k_nearest_neighbors = neighbors[:self.k]
+        return nearest_neighbors
+    
+    
+            
     
          
